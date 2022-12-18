4dab98b5370a9a1a7dbbabd376518dc96281b68f
ReCodee <abhishek27082000@gmail.com>
2022-12-18 16:04:50 +0530

    Project-Initialization

---
+++ Online-Ticketing/src/main/java/com/ticketing/application/OnlineTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.ticketing.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTicketingApplication.class, args);
+	}
+
+}
