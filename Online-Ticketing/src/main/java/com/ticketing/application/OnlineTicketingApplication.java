07588d485f02b12e63b8942001defe9056221446
ReCodee <abhishek27082000@gmail.com>
2022-12-18 19:54:07 +0530

    User-RestAPI

--- Online-Ticketing/src/main/java/com/ticketing/application/OnlineTicketingApplication.java
+++ Online-Ticketing/src/main/java/com/ticketing/application/OnlineTicketingApplication.java
@@ -1,13 +1,29 @@
 package com.ticketing.application;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ticketing.application.model.*;
+import com.ticketing.application.repository.UserRepository;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlineTicketingApplication {
+public class OnlineTicketingApplication implements CommandLineRunner {
 
+	@Autowired
+	private UserRepository userRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineTicketingApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		 this.userRepository.save(new User("Ramesh", "Fadatare", "ramesh@gmail.com"));
+	        this.userRepository.save(new User("Tom", "Cruise", "tom@gmail.com"));
+	        this.userRepository.save(new User("Tony", "Stark", "tony@gmail.com"));
 	}
 
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from  '@angular/common/http';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
